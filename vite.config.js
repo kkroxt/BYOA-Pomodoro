20ba6121a9a560d8c9245fb0c0953e7b148f03bd
Kris Oxtal <krisoxtal@Kriss-Mac-mini.local>
2025-06-10 16:36:37 -0400

    chore: optimize build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/BYOA-Pomodoro/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 }); 
